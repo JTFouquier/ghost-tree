8a87987ca0f6824fc9d64105f166f5168cb1a320
Jennifer Fouquier <jennietf@gmail.com>
2015-02-04 17:24:52 -0700

    silva filtering

---
+++ ghosttree/silva/filter.py
@@ -0,0 +1,28 @@
+import skbio
+
+def fungi_from_fasta(fasta_fh, accession_fh, taxonomy_fh):
+    accession_map = _parse_accession_map(accession_fh)
+    taxonomy_map = _parse_taxonomy_map(taxonomy_fh)
+    for seq in skbio.read(fasta_fh, format="fasta"):
+        map_num = accession_map[seq.id]
+        if map_num in taxonomy_map:
+            yield seq
+
+def _parse_accession_map(accession_fh):
+    accession_map = {}
+    for line in accession_fh:
+        accession, map_num = line.split("\t")
+        if accession in accession_map:
+            raise ValueError("Duplicate accession number %r" % accession)
+        accession_map[accession] = map_num
+    return accession_map
+
+def _parse_taxonomy_map(taxonomy_fh):
+    taxonomy_map = {}
+    for line in taxonomy_fh:
+        taxonomy, map_num, rank = line.split("\t")
+        if rank == "genus" and "Fungi" in taxonomy:
+            if map_num in taxonomy_map:
+                raise ValueError("Duplicate map number %r" % map_num)
+            taxonomy_map[map_num] = taxonomy
+    return taxonomy_map
