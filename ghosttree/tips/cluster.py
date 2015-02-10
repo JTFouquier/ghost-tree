963f778de616cdeff9811b4f9327dc2818d64475
Jennifer Fouquier <jennietf@gmail.com>
2015-02-10 14:55:52 -0800

    add more unit tests and preprocess seqs

--- ghosttree/tips/cluster.py
+++ ghosttree/tips/cluster.py
@@ -1,17 +1,28 @@
 import skbio
+import re
 from skbio import BiologicalSequence
 
 
 """ takes 97_OTUs (at "species" level) and clusters them at a lower level of
 similarity using swarm.
+
+Yields an accession number with a sequence that has been stripped of any
+resricted characters
+
+_1 is a placeholder for the swarm package that requires "_" + abundance.
+But abundance of sequences can also be found in OTU tables.
+
+Arbitrarily replace non ATCG characters with "A" (this is questionable)
 """
 
 
 def preprocess_tip_sequences(species_level_otus_f):
     for seq in skbio.read(species_level_otus_f, format="fasta"):
+        seq = seq.upper()
         if "-" not in seq.id:
             new_seq_id = str(seq.id) + "_1"
-        seq = BiologicalSequence(seq.sequence, id=new_seq_id)
+        new_seq_sequence = re.sub('[^ATCG]', 'A', seq.sequence)
+        seq = BiologicalSequence(new_seq_sequence, id=new_seq_id)
         yield seq
 
 # swarmdir = "/Users/jenniferfouquier/dev/ghost-tree/swarm"
--- ghosttree/tips/tests/test_cluster.py
+++ ghosttree/tips/tests/test_cluster.py
@@ -10,15 +10,25 @@ from ghosttree.tips.cluster import preprocess_tip_sequences
 # requirements set forth by SWARM software)
 class TestClusterTipSequences(unittest.TestCase):
     def setUp(self):
-        self.tips_with_returns = StringIO(tips_with_returns)
+        self.tips_with_normal_format = StringIO(tips_with_normal_format)
+        self.tips_random_characters = StringIO(tips_random_characters)
 
-    def test_tip_sequences_with_returns(self):
-        result = preprocess_tip_sequences(self.tips_with_returns)
+    def test_tip_seqs_normal(self):
+        result = preprocess_tip_sequences(self.tips_with_normal_format)
+        print result
         self.assertEqual(list(result), [BiologicalSequence("ATC",
                                                            id="SSS456_1")])
 
-tips_with_returns = """>SSS456
+    def test_tip_seqs_random_characters(self):
+        result = preprocess_tip_sequences(self.tips_random_characters)
+        self.assertEqual(list(result),
+                         [BiologicalSequence("AGGAAAAA", id="JJJ123_1")])
+
+tips_with_normal_format = """>SSS456
 ATC
+"""
+tips_random_characters = """>JJJ123_1
+AgGS SSP
 """
 
 if __name__ == "__main__":
