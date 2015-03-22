78beeb6375d0f43748e7cd1e0cb53704f037342d
Jennifer Fouquier <jennietf@gmail.com>
2015-03-22 12:28:51 -0700

    add sumaclust to cluster.py; cannot be tested on Travis so remove unit test for cluster.py

--- ghosttree/tips/cluster.py
+++ ghosttree/tips/cluster.py
@@ -1,72 +1,38 @@
-import skbio
-import re
-from skbio import BiologicalSequence
-
-# insert taxonomy file added
+import os
 
 
-def preprocess_tip_sequences(species_level_otus_fh, taxonomy_fh):
-    """Cluster OTUs in small groups for alignment and tree construction.
+def preprocess_tip_sequences(tips_sequences_fh, similarity_threshold,
+                             otu_formatted_fh):
+    """Creates an OTU cluster formatted file from a .fasta sequence file.
 
-    Draft notes:
-
-    Takes 97_OTUs (at "species" level) and clusters them at a lower level of
-    similarity using swarm.
-
-    Yields an accession number with a sequence that has been stripped of
-    characters other than A,T,C or G
-
-    _1 is a placeholder for the swarm package that requires "_" + abundance.
-    But abundance of sequences can also be found in Biome tables.
-
-    Arbitrarily replace non ATCG characters with "A" (this is up for debate,
-    but only changes percent of As ~1 percent)
-
-    Taxonomy file is a tab-delimited text file wih the first column being an
-    accession number and the second column is the taxonomy ranking in the
-    format
-
-    k__Fungi;p__Basidiomycota;c__Agaricomycetes;o__Sebacinales;
-    f__Sebacinaceae;g__unidentified;s__unculturedSebacina
+    Creates an OTU cluster formatted file from a sequence file in .fasta
+    format using Sumaclust (http://metabarcoding.org/sumatra). Sequence file
+    can be from a database or from representative sequences specific to one's
+    own dataset. OTU clusters will be created according to the threshold.
+    The lower the threshold, the more "unidentified" sequences will be
+    accounted for, but this will create larger "tips" groups that form less
+    accurate multiple sequence alignments. Changing the threshold value to a
+    number drastically different from the standard 97 or 99 OTU similarity
+    will alter the resulting tree improving or reducing its utility in
+    diversity analyses and should be performed with caution.
 
     Parameters
-    ----------
-    species_level_otus_fh : filehandle
-        Fasta file containing representative OTUs from a database or dataset.
-        Each sequence identifier must be an accession number
+    __________
+    tips_sequences_fh : filehandle
+        Must be a .fasta formatted file from a database or representative
+        sequences from a dataset. Usually these are 97 or 99 percent OTU
+        sequences.
 
-    Returns
-    -------
-    generator
-        Yields ``skbio.BiologicalSequence`` objects.
+    similarity_threshold : float
+        Value ranging from 0.00 to 1.00. Changing the threshold value to a
+        number drastically different from the standard 97 or 99 OTU similarity
+        will alter the resulting tree improving or reducing its utility in
+        diversity analyses and should be performed with caution.
+
+    otu_formatted_fh : filehandle
+        Name of resulting OTU cluster formatted filehandle.
 
     """
-    build_taxonomy_dic(taxonomy_fh)
-    for seq in skbio.read(species_level_otus_fh, format="fasta"):
-        seq = seq.upper()
-        if "-" not in seq.id:
-            new_seq_id = str(seq.id) + "_1"
-        new_seq_sequence = re.sub('[^ATCG]', 'A', seq.sequence)
-        seq = BiologicalSequence(new_seq_sequence, id=new_seq_id)
-        yield seq
 
-
-def build_taxonomy_dic(taxonomy_fh):
-    taxonomy_dic = {}
-    for line in taxonomy_fh:
-        accession, full_taxonomy_line = line.split("\t")
-        if "g__" not in full_taxonomy_line:
-            raise ValueError("Taxonomy file must contain genera %r" %
-                             full_taxonomy_line)
-        taxonomy_dic[accession] = full_taxonomy_line
-    return taxonomy_dic
-
-# muscledir = "/Applications/muscle"
-
-# ftdir = "/Applications/./FastTree"
-# swarmdir = "/Users/jenniferfouquier/dev/ghost-tree/swarm"
-
-# os.system(""+muscledir+" -in "+inputname+" -out " +name+ "_aligned.fasta
-# -quiet -maxiters 2 -diags1")
-
-# os.system(""+ftdir+" -nt -quiet "+inputname+" > " +name+ "_tree.nwk")
+    os.system("sumaclust -g -f -t "+similarity_threshold+" -O " +
+              "" + otu_formatted_fh + " " + tips_sequences_fh + "")
