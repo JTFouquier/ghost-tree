20f6385297795c8b7e298ed71996bb39ec0860c0
Jennifer Fouquier <jennietf@gmail.com>
2015-03-29 21:10:39 -0700

    I forgot to check Flake8... fixed now

--- ghosttree/filter.py
+++ ghosttree/filter.py
@@ -1,12 +1,12 @@
 
 import skbio
-from skbio.alignment import Alignment
-from skbio.sequence import DNA
 
 import numpy as np
 # unit testing here is going to be difficult. Maybe move these into two
 # functions
 # I made three functions... and yes unit testing is a difficult here....
+
+
 def filter_positions(alignment_fh, maximum_gap_frequency,
                      maximum_position_entropy):
     """Filter gaps and high entropy positions from an alignment."""
@@ -15,10 +15,12 @@ def filter_positions(alignment_fh, maximum_gap_frequency,
     aln = _filter_high_entropy_positions(aln, maximum_position_entropy)
     return aln
 
+
 def _filter_gap_positions(aln, maximum_gap_frequency):
     aln = aln.omit_gap_positions(1.0 - np.finfo(float).eps)
     aln = aln.omit_gap_positions(maximum_gap_frequency)
     return aln
+
 
 def _filter_high_entropy_positions(aln, maximum_position_entropy):
     entropies = aln.position_entropies(nan_on_non_standard_chars=False)
